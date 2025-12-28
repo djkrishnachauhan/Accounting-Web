68ad8c3f101a02fb9e40a21d4f19969cf55a8770
KRISHH CHAUHAN <95200141+djkrishnachauhan@users.noreply.github.com>
2025-12-28 14:46:46 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,22 @@
-import { Menu, User } from "lucide-react";
+import { Menu } from "lucide-react";
 
 export default function Header({ setSidebarOpen }) {
   return (
-    <header className="h-16 bg-white shadow flex items-center justify-between px-4">
-      <button onClick={() => setSidebarOpen(p => !p)}>
-        <Menu />
+    <header className="h-14 bg-gray-300 border-b flex items-center px-4 justify-between">
+      
+      {/* ☰ MENU BUTTON */}
+      <button
+        className="md:hidden"
+        onClick={() => setSidebarOpen(prev => !prev)}
+      >
+        <Menu size={28} />
       </button>
 
-      <h1 className="font-bold text-xl tracking-wide">
+      <h1 className="text-2xl font-bold tracking-wide">
         RAJASTHAN TOOLS
       </h1>
 
-      <User />
+      <div className="w-8 h-8 rounded-full border-2 border-black" />
     </header>
   );
 }
